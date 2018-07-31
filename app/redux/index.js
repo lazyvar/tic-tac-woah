5e2c1f949db6a85744238c165252df9f8fed314e
Mack Hasz <mack@hasz.email>
2018-07-31 17:05:00 -0400

    gameListRedux init

--- app/redux/index.js
+++ app/redux/index.js
@@ -1,9 +1,12 @@
 import { combineReducers } from 'redux'
 
 import * as authRedux from './authRedux'
+import * as gameListRedux from './gameListRedux'
 
 export const reducer = combineReducers({
   auth: authRedux.reducer,
+  gameList: gameListRedux.reducer,
 })
 
 export const authActionCreators = authRedux.actionCreators
+export const gameListActionCreators = gameListRedux.actionCreators
