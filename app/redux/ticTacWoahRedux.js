fdb6e624c47c8898cdf807048edafbbeec4d1281
Mack Hasz <mack@hasz.email>
2018-07-31 15:55:31 -0400

    redux skeleton

---
+++ app/redux/ticTacWoahRedux.js
@@ -0,0 +1,21 @@
+const types = {
+
+}
+
+export const actionCreators = {
+
+}
+
+const initialState = {
+
+}
+
+export const reducer = (state = initialState, action) => {
+  const {type, payload} = action
+
+  switch(type) {
+    default: {
+      return state
+    }
+  }
+}
