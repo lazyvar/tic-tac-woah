3e102681bb05eb89863eb2d1efa7abc9e1757012
Mack Hasz <mack@hasz.email>
2018-08-01 20:20:24 -0400

    correct styling for Square

--- app/components/Square.js
+++ app/components/Square.js
@@ -12,20 +12,11 @@ class Square extends Component {
   selectionStyle = (selection) => {
     switch (selection) {
       case 0:
-        return {
-          ...baseStyle,
-          ...empty
-        }
+        return styles.empty
       case 1:
-        return {
-          ...baseStyle,
-          ...player1
-        }
+        return styles.player1
       case 2:
-        return {
-          ...baseStyle,
-          ...player2
-        }
+        return styles.player2
     }
   }
 
@@ -34,36 +25,35 @@ class Square extends Component {
 
     const logic = new GameLogic(game)
     const board = logic.computeBoard()
-    const squareStyle = this.selectionStyle(board[i][j])
+    const selectionStyle = this.selectionStyle(board[i][j])
 
     return (
-      <View style={squareStyle}>
-        <Text> {i} {j} </Text>
+      <View style={[styles.baseStyle, selectionStyle]}>
+
       </View>
     )
   }
 }
 
-const baseStyle = {
+const styles = StyleSheet.create({
+  baseStyle: {
     borderRadius: 4,
     borderWidth: 0.5,
     height: '90.5%',
     width: '90.5%',
-}
-
-const empty = {
-  backgroundColor: 'white',
-  borderColor: 'white',
-}
-
-const player1 = {
-  backgroundColor: 'steelblue',
-  borderColor: 'steelblue',
-}
-
-const player2 = {
-  backgroundColor: 'firebrick',
-  borderColor: 'firebrick',
-}
+  },
+  empty: {
+    backgroundColor: 'white',
+    borderColor: 'white',
+  },
+  player1: {
+    backgroundColor: 'steelblue',
+    borderColor: 'steelblue',
+  },
+  player2: {
+    backgroundColor: 'firebrick',
+    borderColor: 'firebrick',
+  }
+})
 
 export default connect(mapStateToProps)(Square)
